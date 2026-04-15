7b71b1d692fefa34bd5e84a0a1e740bfbfae56ca
Karthik0484 <karthikdeepshika484@gmail.com>
2026-04-15 10:10:20 +0530

    fixed case sensitive model

--- Indusmind/backend/server.js
+++ Indusmind/backend/server.js
@@ -4,7 +4,7 @@ const cors = require("cors");
 const { exec } = require("child_process");
 
 require("./db"); // DB connection
-const MachineData = require("./models/Machinedata");
+const MachineData = require("./models/MachineData");
 
 const app = express();
 const PORT = process.env.PORT || 5000;
